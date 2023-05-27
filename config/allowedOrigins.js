e3b31965095ded0647f151098c671bc35e6e5c52
Caesar Ngyn <caesarngyn@gmail.com>
2023-05-27 16:22:56 +0700

    middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  'http://localhost:1212',
+  'https://www.google.com'
+
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -5,12 +5,29 @@ require('dotenv').config();
 const port = process.env.PORT || "8085";
 const path = require('path')
 const webRouter = require('./routes/web')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
 //Serving static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public'))) //built-in middleware
+
+//using logger middleware
+app.use(logger) //custom-middleware
+
+//set up cors cross origin resource sharing
+app.use(cors(corsOptions))
+
+//using cookie-parser
+app.use(cookieParser()) //third-party middleware
 
 //set Route
 app.use('/', webRouter)
+
+//parse JSON data
+app.use(express.json())
 
 //Handling 404 Error
 app.all('*', (req, res) => {
@@ -23,6 +40,9 @@ app.all('*', (req, res) => {
     res.type('txt').send('404 Not Found')
   }
 })
+
+//using errorHandler for errer handling middleware
+app.use(errorHandler)
 
 //Set up server
 app.listen(port, () => {
