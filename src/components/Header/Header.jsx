e0c729c91eec43edbc6afe219b52a8c7e2c59b40
Trafunny <trajihi2002@gmail.com>
2023-06-03 02:26:33 +0700

    Edit header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,24 +4,26 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useNavigate, NavLink } from "react-router-dom";
+import './Header.scss'
+
 const Header = () => {
   return (
-    //Code mẫu thanh NavBar
-    //Có thể tham khảo mục HomePage trong src/components/HomePage/HomePage.jsx
-    //để xem cách chia Div Layout
+    // Code mẫu thanh NavBar
+    // Có thể tham khảo mục HomePage trong src/components/HomePage/HomePage.jsx
+    // để xem cách chia Div Layout
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">STeeK English</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="#courses" className="nav-link">Các Khóa Học</NavLink>
-            <NavLink to="#roadmap" className="nav-link">Lộ Trình</NavLink>
-            <NavLink to="#learn" className="nav-link">Học</NavLink>
+            <NavLink to="#courses" className="nav-link">Courses</NavLink>
+            <NavLink to="#roadmap" className="nav-link">Roap Map</NavLink>
+            <NavLink to="#learn" className="nav-link">Study</NavLink>
           </Nav>
           <Nav>
-            {/* <NavLink to="/login" className="nav-link">(Tên tài khoản)</NavLink> */}
-            <NavDropdown title="(Tên tài khoản)" id="basic-nav-dropdown">
+             {/* <NavLink to="/login" className="nav-link">(Tên tài khoản)</NavLink>  */}
+            <NavDropdown title="Account Name" id="basic-nav-dropdown">
               <NavDropdown.Item>
                 Thông tin</NavDropdown.Item>
               <NavDropdown.Item>Đăng xuất</NavDropdown.Item>
@@ -29,7 +31,13 @@ const Header = () => {
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+     </Navbar>
+    
+
+
+
+
+
   );
 }
 
