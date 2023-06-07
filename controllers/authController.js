33a80d693b9598e0c234e3d20f4bf7df57fe4a7f
Caesar Ngyn <caesarngyn@gmail.com>
2023-06-07 17:51:13 +0700

    jwt refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 
 // @desc Login
-// @route POST /auth
+// @route POST /auth/login
 // @access Public
 const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body
@@ -57,7 +57,36 @@ const login = asyncHandler(async (req, res) => {
 // @route GET /auth/refresh
 // @access Public - because access token has expired
 const refresh = (req, res) => {
+  const cookies = req.cookies
 
+  if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' })
+
+  const refreshToken = cookies.jwt
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) return res.status(403).json({ message: 'Forbidden' })
+
+      const foundUser = await User.findOne({ email: decoded.email }).exec()
+
+      if (!foundUser) return res.status(401).json({ message: 'Unauthorized' })
+
+      const accessToken = jwt.sign(
+        {
+          "UserInfo": {
+            "email": foundUser.email,
+            "roles": foundUser.roles
+          }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '15m' }
+      )
+
+      res.json({ accessToken })
+    })
+  )
 }
 
 // @desc Logout
