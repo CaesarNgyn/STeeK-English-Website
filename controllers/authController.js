6db2e38c9355753411db9d36d5aafc72b427d019
Caesar Ngyn <caesarngyn@gmail.com>
2023-06-07 17:56:09 +0700

    jwt logout and cookie cleared

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,7 +93,10 @@ const refresh = (req, res) => {
 // @route POST /auth/logout
 // @access Public - just to clear cookie if exists
 const logout = (req, res) => {
-
+  const cookies = req.cookies
+  if (!cookies?.jwt) return res.sendStatus(204) //No content
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
+  res.json({ message: 'Cookie cleared' })
 }
 
 module.exports = {
