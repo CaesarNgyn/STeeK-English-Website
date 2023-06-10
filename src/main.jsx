585d7f0d86b8b1917c6868448de1e75d5c39ca66
Caesar Ngyn <caesarngyn@gmail.com>
2023-06-10 14:45:25 +0700

    redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,20 @@ import { Provider } from 'react-redux';
 import Layout from './Layout.jsx';
 import Header from './components/Header/Header.jsx';
 import Login from './components/Auth/Login.jsx';
-import store from './redux/store.js';
+import { store, persistor } from './redux/store.js';
 import 'nprogress/nprogress.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store} >
-    <React.StrictMode>
-      <BrowserRouter>
-        <Layout />
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Layout />
 
-      </BrowserRouter>
-    </React.StrictMode>
+        </BrowserRouter>
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice.js'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './slices/userSlice.js';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+// Create the persist config
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+//Combine mutiple reducers
+const rootReducer = combineReducers({
+  user: userReducer,
 });
 
-export default store;
+// Create the persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+
+// Create the Redux store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+// Create the persisted store
+const persistor = persistStore(store);
+
+export { store, persistor };
