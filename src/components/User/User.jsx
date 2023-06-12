3433fd8898a1f8b075b8f82e76fc5413ee97e97b
Trafunny <trajihi2002@gmail.com>
2023-06-12 20:58:01 +0700

    update /home , and add img for asset

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,3 +1,4 @@
+
 import './User.scss'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -6,32 +7,47 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import { useSelector } from 'react-redux';
 import Course from './Course/Course';
-
-
+import userimg from '../../assets/user-img.png'
+import traimg from '../../assets/traimg.png'
+import Carousel from 'react-bootstrap/Carousel';
 const User = () => {
   const isAuthenticated = useSelector(state => state.user.isAuthenticated)
   const userAccount = useSelector(state => state.user.account)
-
   // console.log("is auth: ", isAuthenticated, "useraccount: ", userAccount)
   return (
-
     <div className='user-container'>
-      <h1 className='welcome'> Mua khóa học Hot nhất ngay!</h1>
+      {/* <h1 className='welcome'> Mua khóa học Hot nhất ngay!</h1> */}
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={userimg}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={traimg}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+      </Carousel>
       <div className='beginner-container'>
         <h1 className='title'>
           Khóa học dành cho người mới bắt đầu
         </h1>
-        <div className='courses'>
+        <div className='courses row'>
+          <Course />
           <Course />
           <Course />
         </div>
-
       </div>
       <div className='toeic-container'>
         <h1 className='title'>
           Khóa học Toeic
         </h1>
-        <div className='courses'>
+        <div className='courses row'>
           <Course />
           <Course />
         </div>
@@ -40,7 +56,7 @@ const User = () => {
         <h1 className='title'>
           Khóa học Ielts
         </h1>
-        <div className='courses'>
+        <div className='courses row'>
           <Course />
           <Course />
         </div>
@@ -49,9 +65,8 @@ const User = () => {
 
 
 
-
     </div>
   )
 }
-
 export default User;
+
