4601a1faaac8a8a4215dfba552c71cf85493e6f7
trafunny <90977262+trafunny@users.noreply.github.com>
2023-06-12 07:29:13 +0700

    Update Study.jsx

--- src/components/User/Study.jsx
+++ src/components/User/Study.jsx
@@ -1,14 +1,53 @@
 import './Study.scss'
-
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Study = () => {
   return (
     //2. Route hiện bên trong khoá học khi ng dùng học
-    <div>
+    <div className='study-content'>
 
-      Study
+      <div className="container row">
+        {/*======================== LIST ITEM =====================*/}
+        <div className="side-bar col-lg-3">
+          <ListGroup defaultActiveKey="#link1">
+            <ListGroup.Item action href="#link1" className='list-title'>
+              TOEIC 250
+            </ListGroup.Item>
+            <ListGroup.Item action href="#link2" >
+              Bai 1
+            </ListGroup.Item>
+            <ListGroup.Item action href="#link2" disabled>
+              Bai 2
+            </ListGroup.Item>
+            <ListGroup.Item action href="#link2" disabled>
+              Bai 3
+            </ListGroup.Item>
+            <ListGroup.Item action href="#link2" disabled>
+              Bai 4
+            </ListGroup.Item>
+
+          </ListGroup>
+
+        </div>
+        {/* ===================== SHOW VIDEO ======================== */}
+        <div className="side-content col-lg-9">
+          <iframe width="95%" height="420px" src="https://www.youtube.com/embed/tPn3qf7ZA6c"
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+          <h3>Toeic 250</h3>
+          <p>Tham gia các cộng đồng để cùng học hỏi, chia sẻ </p>
+          <ul>
+            <li>Fanpage: <a href="https://www.facebook.com/f8vnofficial" target="_blank" rel="noreferrer">https://www.facebook.com/f8vnofficial</a></li>
+            <li>Group: <a href="https://www.facebook.com/groups/649972919142215" target="_blank" rel="noreferrer">https://www.facebook.com/groups/649972919142215</a></li>
+            <li>Youtube: <a href="/external-url?continue=https%3A%2F%2Fwww.youtube.com%2FF8VNOfficial" target="_blank" rel="noreferrer">https://www.youtube.com/F8VNOfficial</a></li>
+          </ul>
+        </div>
+      </div>
+
+
     </div>
   )
 }
 
 export default Study;
+
