ca0f340b4080aa087e9e9b4160c7e2feaabe36de
Trafunny <trajihi2002@gmail.com>
2023-06-12 19:05:19 +0700

    ignore cmt all file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,33 @@
-// import { configureStore, combineReducers } from '@reduxjs/toolkit';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import userReducer from './slices/userSlice.js';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './slices/userSlice.js';
 
-// // Create the persist config
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-// //Combine mutiple reducers
-// const rootReducer = combineReducers({
-//   user: userReducer,
-// });
+// Create the persist config
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+//Combine mutiple reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+});
 
-// // Create the persisted reducer
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+// Create the persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 
-// // Create the Redux store
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-// });
+// Create the Redux store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-// // Create the persisted store
-// const persistor = persistStore(store);
+// Create the persisted store
+const persistor = persistStore(store);
 
-// export { store, persistor };
+export { store, persistor };
