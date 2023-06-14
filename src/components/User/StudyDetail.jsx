c93342ebb07ca9c7c2c39a82801d889692da69b2
Trafunny <trajihi2002@gmail.com>
2023-06-14 22:46:33 +0700

    css for studyDetail

--- src/components/User/StudyDetail.jsx
+++ src/components/User/StudyDetail.jsx
@@ -22,70 +22,80 @@ const StudyDetail = () => {
 
   return (
     //2. Route hiện bên trong khoá học khi ng dùng học
-    <div className='study-content'>
-      <div className="container row">
-        {/*======================== LIST ITEM =====================*/}
-        <div className="side-bar col-lg-3">
-          <ListGroup defaultActiveKey="#link1">
-            <ListGroup.Item
-              action href="#link1"
-              className='list-title'
-              disabled
-            >
-              {course.title}
-            </ListGroup.Item>
+    <div className='studyDetail-content row'>
+
+      {/*======================== LIST ITEM =====================*/}
+      <div className="side-bar col-lg-3">
+        <ListGroup defaultActiveKey="#link1">
+          <ListGroup.Item
+            action href="#link1"
+            className='list-title'
+            disabled
+          >
+            {/* {course.title} */}
+            Các bài học
+          </ListGroup.Item>
 
 
-            {course && course.listVideo?.length > 0 &&
-              course.listVideo.map((video, index) => (
-                <ListGroup.Item
-                  key={`video-${index}`}
-                  action
-                  onClick={() => handleOnClickLesson(video)}
-                  href={currentVideo === video ? '#link1' : ''}
-                  disabled={currentVideo === video ? true : false}
-                >
-                  Lesson {index + 1}
-                </ListGroup.Item>
-              ))}
+          {course && course.listVideo?.length > 0 &&
+            course.listVideo.map((video, index) => (
+              <ListGroup.Item
+                key={`video-${index}`}
+                action
+                onClick={() => handleOnClickLesson(video)}
+                href={currentVideo === video ? '#link1' : ''}
+                disabled={currentVideo === video ? true : false}
+              >
+                Lesson {index + 1}
+              </ListGroup.Item>
+            ))}
 
 
-          </ListGroup>
+        </ListGroup>
 
-        </div>
-        {/* ===================== SHOW VIDEO ======================== */}
-        <div className="side-content col-lg-9">
+      </div>
+      {/* ===================== SHOW VIDEO ======================== */}
+      <div className="side-content col-lg-9">
+        <div className="course-detail">
           <iframe
-            width="105%"
-            height="450px"
+            width="100%"
+            height="500vh"
             src={`https://www.youtube.com/embed/${videoURL}`}
             title="YouTube video player"
             allowFullScreen></iframe>
+
           <h3 className='course-title'>{course.title}</h3>
           <p>{course.description} </p>
+        </div>
+
+        <div className="social">
+          <p>Tham gia các cộng đồng để cùng học hỏi, chia sẻ và "thám thính" xem STeeK sắp có gì mới nhé! </p>
           <ul>
             <li>Fanpage:
-              <a href="https://www.facebook.com/f8vnofficial"
+              <a href="https://www.facebook.com/tra.victor/"
                 target="_blank"
                 rel="noreferrer">
-                https://www.facebook.com/f8vnofficial
+                https://www.facebook.com/tra.victor/
               </a>
             </li>
             <li>Group:
-              <a href="https://www.facebook.com/groups/649972919142215"
+              <a href="https://www.facebook.com/khanhrussian"
                 target="_blank" rel="noreferrer">
-                https://www.facebook.com/groups/649972919142215</a>
+                https://www.facebook.com/khanhrussian</a>
             </li>
             <li>Youtube:
-              <a href="/external-url?continue=https%3A%2F%2Fwww.youtube.com%2FF8VNOfficial"
+              <a href="https://www.youtube.com/watch?v=Exwbdxba2Mo"
                 target="_blank"
                 rel="noreferrer">
-                https://www.youtube.com/F8VNOfficial
+                https://www.youtube.com/watch?v=Exwbdxba2Mo
               </a>
             </li>
           </ul>
+
         </div>
+
       </div>
+
 
 
     </div>
