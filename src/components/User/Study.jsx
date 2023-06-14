10416339fc76eecd770fef46edc7e57bce9b552e
Trafunny <trajihi2002@gmail.com>
2023-06-15 01:24:10 +0700

    update study

--- src/components/User/Study.jsx
+++ src/components/User/Study.jsx
@@ -34,12 +34,46 @@ const Study = () => {
   return (
     //2. Route hiện bên trong khoá học khi ng dùng học
     <div className='study-content'>
-      <h1 className='title'>Các khóa học đã mua</h1>
-      {courses && courses.length > 0 && courses.map((course, index) => (
-        <div key={`course-${index}`} className='courses'>
-          <Course course={course} isBought={isBought} />
+      <div className="container">
+
+        {/* ================ Header =================== */}
+        <header>
+          <h1 className='title'>Các khóa học đã mua</h1>
+          <p>Các khóa học được thiết kế phù hợp cho cả người mới, nhiều khóa học miễn phí, chất lượng, nội dung dễ hiểu.</p>
+        </header>
+
+        <h1 className='title'>Hãy bắt đầu học thôi </h1>
+          {/* ==================== Direction ============= */}
+        <div className="direction">
+          <div className="section">
+            <span className="icon"><i class="fad fa-search-location"></i></span>
+            <p className="content-step line">1. Quan sát lộ trình </p>
+            
+          </div>
+          <div className="section">
+            <span className="icon"><i class="fad fa-pencil"></i></span>
+            <p className="content-step line">2. Xác định thứ tự các khóa </p>
+            
+          </div>
+          <div className="section">
+            <span className="icon"><i class="far fa-bullseye-arrow"></i></span>
+            <p className="content-step">3. Let's go</p>
+            
+          </div>
         </div>
-      ))}
+
+          {/* ======================= Registered-Courses =================== */}
+        <div className="registered-course row">
+          {courses && courses.length > 0 && courses.map((course, index) => (
+            // <div key={`course-${index}`} className='courses col-lg-4'>
+            <Course course={course} isBought={isBought} />
+            // {/* </div> */}
+          ))}
+
+        </div>
+
+      </div>
+
 
 
 
